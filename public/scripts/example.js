9cbfa2db95f211175bdee08694469c19f4119cb9
Kristina Entzel <kren7615@colorado.edu>
2015-03-09 17:28:43 -0600

    completed milestone 3 and 4

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -38,21 +38,31 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+ handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.getDOMNode().value.trim();
+    var text = this.refs.text.getDOMNode().value.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    this.refs.author.getDOMNode().value = '';
+    this.refs.text.getDOMNode().value = '';
+  },
   render: function() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+         <input type="text" placeholder="Your name" ref="author" />
+         <input type="text" placeholder="Say something..." ref="text"/>
+         <input type="submit" value="Post" />
+       </form>
     );
   }
 });
 
 
 var CommentBox = React.createClass({
-  getInitialState: function() {
-      return {data: []};
-  },
- componentDidMount: function() {
+ loadCommentsFromServer: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -64,17 +74,41 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
+  handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+      $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+      return {data: []};
+  },
+  componentDidMount: function() {
+   this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+ },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
 });
 React.render(
-  <CommentBox url="comments.json" />,
+  <CommentBox url="comments.json" pollInterval={2000} />,
   document.getElementById('content')
 )
